114c7b283956bdd8b69e12dbc80cbec59d2b7d20
Martine Lenders <m.lenders@fu-berlin.de>
2021-12-01 18:53:54 +0100

    script/plots: rework c4trans script to use CDF

--- scripts/plots/plot_load_trans.py
+++ scripts/plots/plot_load_trans.py
@@ -18,8 +18,10 @@ import numpy
 
 try:
     from . import plot_common as pc
+    from . import plot_load_cdf
 except ImportError:
     import plot_common as pc
+    import plot_load_cdf
 
 __author__ = "Martine S. Lenders"
 __copyright__ = "Copyright 2021 Freie Universität Berlin"
@@ -30,6 +32,38 @@ __email__ = "m.lenders@fu-berlin.de"
 RETRIES = 4
 TIMEOUT_MS = 2000
 COAP_RANDOM_FACTOR_1000 = 1500
+
+
+def files_to_tables(files, transport, record, method):
+    tables = {}
+    for match, filename in files[-pc.RUNS :]:
+        if match["record"] is None and record != pc.RECORD_TYPE_DEFAULT:
+            continue
+        if (
+            transport in pc.COAP_TRANSPORTS
+            and match["method"] is None
+            and method != pc.COAP_METHOD_DEFAULT
+        ):
+            continue
+        filename = os.path.join(pc.DATA_PATH, filename)
+        with open(filename, encoding="utf-8") as timesfile:
+            reader = csv.DictReader(timesfile, delimiter=";")
+            timestamp = int(match["timestamp"])
+            if timestamp not in tables:
+                tables[timestamp] = {}
+            table = tables[timestamp]
+            for row in reader:
+                id_ = int(row["id"])
+                if id_ in table:
+                    table[id_].update({k: v for k, v in row.items() if v})
+                else:
+                    table[id_] = row
+    for timestamp in tables:
+        tables[timestamp] = sorted(
+            (v for v in tables[timestamp].values()),
+            key=lambda v: float(v["query_time"]),
+        )
+    return tables
 
 
 def process_data(
@@ -51,44 +85,29 @@ def process_data(
         avg_queries_per_sec,
         record,
     )
+    tables = files_to_tables(files, transport, method, record)
     transmissions = []
-    unauthorized = []
-    for match, filename in files[-pc.RUNS :]:
-        if match["record"] is None and record != pc.RECORD_TYPE_DEFAULT:
-            continue
-        if (
-            transport in pc.COAP_TRANSPORTS
-            and match["method"] is None
-            and method != pc.COAP_METHOD_DEFAULT
-        ):
-            continue
-        filename = os.path.join(pc.DATA_PATH, filename)
-        with open(filename, encoding="utf-8") as timesfile:
-            reader = csv.DictReader(timesfile, delimiter=";")
-            base_id = None
-            base_time = None
-            last_id = None
-            for row in reader:
-                base_id, base_time = pc.normalize_times_and_ids(row, base_id, base_time)
-                if last_id is not None and last_id < (row["id"] - 1):
-                    # A query was skipped
-                    transmissions.append((numpy.nan, numpy.nan))
-                else:
-                    if row.get("transmissions"):
-                        for transmission in row["transmissions"]:
-                            transmissions.append(
-                                (
-                                    row["query_time"],
-                                    transmission - row["query_time"],
-                                )
+    for timestamp in tables:
+        base_id = None
+        base_time = None
+        last_id = None
+        for row in tables[timestamp]:
+            base_id, base_time = pc.normalize_times_and_ids(row, base_id, base_time)
+            if last_id is not None and last_id < (row["id"] - 1):
+                # A query was skipped
+                transmissions.append((numpy.nan, numpy.nan))
+            else:
+                if row.get("transmissions"):
+                    for transmission in row["transmissions"]:
+                        transmissions.append(
+                            (
+                                row["query_time"],
+                                transmission - row["query_time"],
                             )
-                    else:
-                        transmissions.append((row["query_time"], numpy.nan))
-                    if row.get("unauth_time"):
-                        unauthorized.append(
-                            (row["query_time"], row["unauth_time"] - row["query_time"])
                         )
-    return numpy.array(transmissions), numpy.array(unauthorized)
+                else:
+                    transmissions.append((row["query_time"], numpy.nan))
+    return numpy.array(transmissions)
 
 
 def mark_exp_retrans(ax):
@@ -117,12 +136,12 @@ def mark_exp_retrans(ax):
 
 
 def label_plot(ax, xmax, ymax, transport, method, time):
-    ax.set_xlabel("Experiment duration [s]")
+    ax.set_xlabel("Query sent timestamp [s]")
     ax.set_xlim((0, xmax))
     ax.set_xticks(numpy.arange(0, xmax + 1, step=2))
-    ax.set_ylabel("Time since query send [s]")
+    ax.set_ylabel("Time since query sent [s]")
     ax.set_ylim((0, ymax))
-    ax.set_yticks(numpy.arange(0, ymax + 1, step=2))
+    ax.set_yticks(numpy.arange(0, ymax + 1, step=5))
     ax.text(
         xmax - 0.1,
         ymax - 0.1,
@@ -146,10 +165,15 @@ def main():  # noqa: C901
                 for time, queries in pc.RESPONSE_DELAYS:
                     for avg_queries_per_sec in pc.AVG_QUERIES_PER_SEC:
                         fig = matplotlib.pyplot.figure(figsize=(4, 13 / 4))
-                        axs = [matplotlib.pyplot.gca()]
-                        axs.append(matplotlib.pyplot.twiny())
-                        bins = mark_exp_retrans(axs[0])
-                        transmissions, unauthorized = process_data(
+                        axs = fig.subplots(
+                            1,
+                            2,
+                            sharey=True,
+                            gridspec_kw={"wspace": 0.1, "width_ratios": [4, 1]},
+                        )
+                        for ax in axs:
+                            mark_exp_retrans(ax)
+                        transmissions = process_data(
                             transport,
                             method,
                             time,
@@ -165,34 +189,32 @@ def main():  # noqa: C901
                             transmissions[:, 0],
                             transmissions[:, 1],
                             s=2,
-                            marker=".",
+                            marker="o",
+                            label="Transport transmissions",
+                            alpha=0.8,
+                            **pc.TRANSPORTS_STYLE[transport],
+                        )
+                        if len(transmissions[:, 1]) == 0:
+                            continue
+                        x, y = plot_load_cdf.cdf(transmissions[:, 1])
+                        axs[1].plot(
+                            y,
+                            x,
                             label="Transport transmissions",
                             **pc.TRANSPORTS_STYLE[transport],
                         )
-                        if len(unauthorized) > 0:
-                            assert unauthorized[:, 0].max() < transmissions[:, 0].max()
-                            assert unauthorized[:, 1].max() < transmissions[:, 1].max()
-                            axs[0].scatter(
-                                unauthorized[:, 0],
-                                unauthorized[:, 1],
-                                marker="x",
-                                label="4.01 Unauthorized Response",
-                                **pc.TRANSPORTS_STYLE[transport],
-                            )
-                            # axs[0].legend(loc="upper left")
-                        axs[1].hist(
-                            transmissions[:, 1],
-                            bins=bins,
-                            density=True,
-                            orientation="horizontal",
-                            alpha=0.5,
-                            **pc.TRANSPORTS_STYLE[transport],
-                        )
-                        axs[1].set_xlabel("Density")
-                        axs[1].set_xticks(numpy.arange(5, -1, step=-1))
+                        axs[1].set_xlabel("CDF")
+                        axs[1].set_xticks(numpy.arange(0, 1.5, step=0.5))
                         mx1.append(axs[1].get_xlim()[1])
-                        axs[1].set_xlim((5, 0))
-                        label_plot(axs[0], 11, 21, transport, method, time)
+                        axs[1].set_xlim((0, 1))
+                        label_plot(
+                            axs[0],
+                            11 if avg_queries_per_sec == 10 else 21,
+                            25,
+                            transport,
+                            method,
+                            time,
+                        )
                         fig.tight_layout()
                         for ext in ["pgf", "svg"]:
                             fig.savefig(
