9c916fb38e32017494bc41c6cc7d93ac755e3d94
Martine Lenders <m.lenders@fu-berlin.de>
2022-01-27 12:56:50 +0100

    plots: add script to analyze CNAME dependencies from MonIoTR data set

---
+++ scripts/plots/plot_iot_data_cname.py
@@ -0,0 +1,199 @@
+#! /usr/bin/env python
+#
+# Copyright (C) 2022 Freie Universität Berlin
+#
+# This file is subject to the terms and conditions of the GNU Lesser
+# General Public License v2.1. See the file LICENSE in the top level
+# directory for more details.
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+import argparse
+import ast
+import re
+import os
+
+import matplotlib.pyplot
+import networkx
+import numpy
+import pandas
+
+from networkx.drawing.nx_pydot import write_dot as networkx_write_dot
+
+try:
+    from . import plot_common as pc
+    from . import plot_iot_data_name_lens as name_len
+    from . import plot_iot_data_hostname_lens as hostname_len
+except ImportError:
+    import plot_common as pc
+    import plot_iot_data_name_lens as name_len
+    import plot_iot_data_hostname_lens as hostname_len
+
+__author__ = "Martine S. Lenders"
+__copyright__ = "Copyright 2022 Freie Universität Berlin"
+__license__ = "LGPL v2.1"
+__email__ = "m.lenders@fu-berlin.de"
+
+
+CDNS = [
+    "amazonaws.com",
+    "arrayent.com",
+    "cloudapp.net",
+    "cloudfront.net.",
+    "facebook.com",
+    "samsung-svoice.com",
+    "samsungcloudsolution.net",
+]
+
+
+def pseudonize_hostname(name):
+    name = name.strip(".")
+    if (
+        name.endswith(".local")
+        or name.endswith(".dada.lab")
+        or name.endswith(".moniotr.lab")
+    ):
+        name = re.sub(
+            "[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}",
+            "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX",
+            name,
+        )
+        name = re.sub("[0-9a-f]{2}(:[0-9a-f]{2}){5}", "XX:XX:XX:XX:XX:XX", name)
+        print(name)
+    if ".awsdns-" in name:
+        name = re.sub(r"awsdns-\d+\.([^.]+)$", r"awsdns-X.\1", name)
+    hostname = hostname_len.extract_hostname(name)
+    pseudonized = re.sub(r"\d+", "X", hostname)
+    return name.replace(hostname, pseudonized)
+
+
+def pseudonize_ipv4_address(address):
+    if address.startswith("192.168.") or address.startswith("10."):
+        return "Local IPv4"
+    return "Global IPv4"
+
+
+def pseudonize_ipv6_address(address):
+    if address.startswith("fe80:"):
+        return "Link local IPv6"
+    if address.startswith("fc") or address.startswith("fd"):
+        return "IPv6 ULA"
+    return "Global IPv6"
+
+
+def pseudonize(cnames):
+    mapping = {}
+    for node in cnames.nodes:
+        node_type = cnames.nodes[node]["type"]
+        if node_type == "ipv4":
+            mapping[node] = pseudonize_ipv4_address(node)
+        elif node_type == "ipv6":
+            mapping[node] = pseudonize_ipv6_address(node)
+        elif node_type == "name":
+            mapping[node] = pseudonize_hostname(node)
+        else:
+            assert node_type in ["ipv4", "ipv6", "name"]
+    return networkx.relabel_nodes(cnames, mapping)
+
+
+def transform_to_edgelist(name, entry):
+    if not entry:
+        return []
+    res = []
+    for ename in entry:
+        if name in ["A", "AAAA"]:
+            continue
+        res.extend([(name, ename)] + transform_to_edgelist(ename, entry[ename]))
+    return res
+
+
+def get_cname_chain_lengths(cnames, filt_name):
+    cname_chain_lengths = []
+    subgraphs = [
+        cnames.subgraph(n).copy() for n in networkx.weakly_connected_components(cnames)
+    ]
+    roots = set()
+    leaves = set()
+    for graph in subgraphs:
+        for node in graph.nodes:
+            if graph.in_degree(node) == 0:
+                roots.add(node)
+            elif graph.out_degree(node) == 0 or all(
+                graph.nodes[neighbor]["type"] in ["ipv4", "ipv6"]
+                for neighbor in graph.neighbors(node)
+            ):
+                if graph.nodes[node]["type"] in ["ipv4", "ipv6"]:
+                    continue
+                leaves.add(node)
+    for root in roots:
+        for leaf in leaves:
+            try:
+                shortest_path = networkx.shortest_path(cnames, root, leaf)
+            except networkx.NetworkXNoPath:
+                continue
+            sp_length = len(shortest_path) - 1
+            if sp_length == 3:
+                print(" -> ".join(str(n) for n in shortest_path))
+                pass
+            cname_chain_lengths.append(sp_length)
+    return cname_chain_lengths
+
+
+def main():
+    matplotlib.style.use(os.path.join(pc.SCRIPT_PATH, "mlenders_usenix.mplstyle"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("iot_data_csv")
+    args = parser.parse_args()
+    for filt_name, filt in name_len.FILTERS:
+        if "qd_an_only" in filt_name or "no_mdns" in filt_name:
+            continue
+        df = pandas.read_csv(args.iot_data_csv)
+        df = name_len.filter_data_frame(df, filt)
+        cnames = networkx.DiGraph()
+        for _, row in df[
+            ((df["type"] == "A") | (df["type"] == "AAAA") | (df["type"] == "CNAME"))
+            & (df["section"] != "qd")
+        ][["type", "name", "rdata"]].iterrows():
+            name = row["name"].lower()
+            typ = row["type"]
+            if typ == "CNAME":
+                # CNAME rdata is present in Python binary string representation
+                cname = ast.literal_eval(row["rdata"]).decode().lower()
+                cnames.add_edge(name, cname, label=typ)
+                cnames.nodes[name]["type"] = "name"
+                cnames.nodes[cname]["type"] = "name"
+            elif typ in ["A", "AAAA"]:
+                cnames.add_edge(name, row["rdata"], label=typ)
+                cnames.nodes[name]["type"] = "name"
+                cnames.nodes[row["rdata"]]["type"] = "ipv4" if typ == "A" else "ipv6"
+        if not len(cnames.nodes):
+            continue
+        chain_lengths = numpy.array(get_cname_chain_lengths(cnames, filt_name))
+        networkx_write_dot(
+            pseudonize(cnames),
+            os.path.join(pc.DATA_PATH, f"iot-data-cname-chains-{filt_name}.dot"),
+        )
+        bins = chain_lengths.max() - chain_lengths.min()
+        matplotlib.pyplot.hist(chain_lengths, bins=bins, density=True, histtype="step")
+        matplotlib.pyplot.xticks(numpy.arange(0, 6, 1))
+        matplotlib.pyplot.xlim((0, 5))
+        matplotlib.pyplot.xlabel(r"CNAME chain lengths [\# linked CNAME records]")
+        matplotlib.pyplot.ylim((-0.01, 1.01))
+        matplotlib.pyplot.ylabel("Density")
+        matplotlib.pyplot.tight_layout()
+        for ext in pc.OUTPUT_FORMATS:
+            matplotlib.pyplot.savefig(
+                os.path.join(
+                    pc.DATA_PATH,
+                    f"iot-data-cname-chain-lens-{filt_name}.{ext}",
+                ),
+                bbox_inches="tight",
+                pad_inches=0.01,
+            )
+        matplotlib.pyplot.clf()
+
+
+if __name__ == "__main__":
+    main()
