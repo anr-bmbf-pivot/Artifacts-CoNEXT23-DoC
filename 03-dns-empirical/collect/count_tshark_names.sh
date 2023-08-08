ce70817d0af50e09fabce82f8e6c73a9ee474c9d
Martine Lenders <m.lenders@fu-berlin.de>
2023-08-08 10:55:16 +0200

    count_tshark_names.sh: make runnable again

--- 03-dns-empirical/collect/count_tshark_names.sh
+++ 03-dns-empirical/collect/count_tshark_names.sh
@@ -6,11 +6,15 @@
 # Distributed under terms of the MIT license.
 #
 
-PROCS=$(grep -c %p'^processor' /proc/cpuinfo)
+PROCS=$(grep -c '^processor' /proc/cpuinfo)
 EXCLUDED_DEVICES="(Gateway|AndroidTablet|iPhone|iPad|MyCloudEX2Ultra|NintendoSwitch|PlayStation4|TP-LinkWiFiPlug|UbuntuDesktop|XboxOneX)"
-DEVICE_MAPPING="doi-10.1109-SP.2019.00013-iot-data/device_mapping.csv"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+DATA_DIR="$(readlink -f "${SCRIPT_DIR}/../results")"
+DEVICE_MAPPING="${DATA_DIR}/doi-10.1109-SP.2019.00013-iot-data/device_mapping.csv"
 
-ls ${DEVICE_MAPPING} &> /dev/null || (echo "Need device mapping CSV at ${DEVICE_MAPPING}"; exit 1) || exit 1
+export DATA_DIR
+
+ls "${DEVICE_MAPPING}" &> /dev/null || (echo "Need device mapping CSV at ${DEVICE_MAPPING}"; exit 1) || exit 1
 
 parse_pcap() {
     no=$(echo "$1" | cut -d';' -f1 | xargs printf "%05d")
@@ -19,17 +23,17 @@ parse_pcap() {
     tshark -Y "dns.qry.name" -r "$pcap" -Tfields \
         -e _ws.col."Source" -e _ws.col."Destination" -e dns.qry.name \
         -E aggregator='|' -E separator=';' | sed "s/^/${dataset};/" \
-        > "names_addr_w_mdns/${no}.csv"
+        > "${DATA_DIR}/names_addr_w_mdns/${no}.csv"
 }
 
 export -f parse_pcap
 
-mkdir -p names_addr_w_mdns
-find {yourthings,iotfinder,moniotr} -name "*.pcap" -o -name "eth1-*" | nl -s';' | \
+mkdir -p "${DATA_DIR}/names_addr_w_mdns"
+find ${DATA_DIR}/{yourthings,iotfinder,moniotr} -name "*.pcap" -o -name "eth1-*" | nl -s';' | \
     parallel -j"${PROCS}" parse_pcap
-cat names_addr_w_mdns/[0-9][0-9][0-9][0-9][0-9].csv > names_addr_w_mdns.csv
+cat "${DATA_DIR}/names_addr_w_mdns/"[0-9][0-9][0-9][0-9][0-9].csv > "${DATA_DIR}/names_addr_w_mdns.csv"
 echo -n "All devices: "
-cut -d';' -f4 names_addr_w_mdns.csv | tr '|' '\n' | tr '[:upper:]' '[:lower:]' | sort -u | wc -l
+cut -d';' -f4 "${DATA_DIR}/names_addr_w_mdns.csv" | tr '|' '\n' | tr '[:upper:]' '[:lower:]' | sort -u | wc -l
 
 INCLUDED_ADDRS=$(
     awk -F, '$1 !~ /'"\<${EXCLUDED_DEVICES}"'\>/ {print $2}' "${DEVICE_MAPPING}" | \
@@ -37,6 +41,6 @@ INCLUDED_ADDRS=$(
 )
 awk -F';' \
     '$1 ~ /moniotr/ || $2 ~ /^'"${INCLUDED_ADDRS}"'$/ || $3 ~ /^'"${INCLUDED_ADDRS}"'$/ {print tolower($4)}' \
-    names_addr_w_mdns.csv > names_w_mdns_filtered.csv
+    "${DATA_DIR}/names_addr_w_mdns.csv" > "${DATA_DIR}/names_w_mdns_filtered.csv"
 echo -n "w/o excluded devices: "
-tr '|' '\n' < names_w_mdns_filtered.csv | sort -u | wc -l
+tr '|' '\n' < "${DATA_DIR}/names_w_mdns_filtered.csv" | sort -u | wc -l
